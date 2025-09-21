f0e952fa6e809b3f4ffca807090518bcbdc7c8c2
shorins <mighty.shorin@yandex.ru>
2025-09-21 21:51:58 +0300

    описана модель по Петиному интерфейсу

--- backend/search_service/app/models.py
+++ backend/search_service/app/models.py
@@ -3,15 +3,13 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
-# class Product(SQLModel, table=True):
-#     id: Optional[int] = Field(default=None, primary_key=True)
-#     name: str = Field(index=True)
-#     category: str
-#     brand: Optional[str] = None
-#     volume: Optional[str] = None
-#     percentage: Optional[float] = None
-
-# Важно: Это пример. Тебе нужно будет создать здесь модели,
-# которые соответствуют реальным данным о товарах.
-# После того как ты создашь или изменишь эту модель, нужно будет создать миграцию Alembic.
-
+class Product(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)  # index=True ускоряет поиск по имени
+    price: float
+    old_price: Optional[float] = None
+    image_url: str  # В БД лучше хранить URL картинки, а не саму картинку
+    rating: float
+    reviews_count: int
+    category: str = Field(index=True) # index=True ускоряет фильтрацию по категории
+    in_stock: bool
