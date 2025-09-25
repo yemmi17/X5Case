e505b48f28a5cd1649fb44c26a038dbd5fb19bfa
shorins <mighty.shorin@yandex.ru>
2025-09-25 19:46:06 +0300

    настроен dockerfile ml сервиса для запуска сервиса чисто под хакатон с одним сервисом

--- ml/app/main.py
+++ ml/app/main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from fastapi.concurrency import run_in_threadpool
 from app.schemas import TextInput, Response
 from app.logic import extract_entities_for_backend
 import uvicorn
 
 app = FastAPI(title="ML Entity Extraction Service")
 
-@app.post("/extract_entities", response_model=Response)
-def extract_entities_endpoint(input_data: TextInput):
+# Превращаем эндпоинт в асинхронный (async def)
+@app.post("/api/predict", response_model=Response)
+async def extract_entities_endpoint(input_data: TextInput = Body(...)):
     """Принимает текст и возвращает массив сущностей"""
-    return extract_entities_for_backend(input_data.text)
+
+    if not input_data.text:
+        return Response(entities=[])
+
+    # Вызываем тяжелую функцию в отдельном потоке
+    result = await run_in_threadpool(extract_entities_for_backend, input_data.text)
+    return result
